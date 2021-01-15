894600b178e884c08487e3cd1b5bf145943efbb7
isaacs <i@izs.me>
2021-01-14 16:50:39 -0800

    have to create file in post bootstrap, even though it's not full until post build
    Also, need the tests to reset after each one

--- test/cases/common.js
+++ test/cases/common.js
@@ -10,6 +10,7 @@ const test = exports.test = async (node, ast, width) =>
     markdownNode: node,
     markdownAST: ast,
   }, { maxWidth: width }).then(() => {
+    plugin.onPostBootstrap()
     plugin.onPostBuild({}, { maxWidth: width })
     ast.t.matchSnapshot(fs.readFileSync(css, 'utf8'), 'photoset css')
   })
